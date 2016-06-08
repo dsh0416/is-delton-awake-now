0a63462e8ae02575bb8e9db19f4d67d0d6345bc4
Delton Ding <dsh0416@gmail.com>
2016-06-09 06:13:57 +0800

    Config for everything

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require 'bundler'
 Bundler.require
+require 'yaml'
 
 configure {set :server, :puma}
+$config =  YAML.load_file('./config.yaml')
+puts $config
 
 $authority = JSON.parse(File.read('./config.json'))['authority']
-# Example Config
-# {"authority":["admin","admin"]}
 
 helpers do
   def protected!
@@ -16,26 +17,19 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == $authority
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [$config['authority']['username'], $config['authority']['password']]
   end
 end
 
-result = false
+result = $config['option'][$config['default']]
 
 get '/' do
-  erb :index, locals: {result: result}
+  erb :index, locals: {result: result, title: $config['title']}
 end
 
-get '/awake' do
+get '/:option' do |option|
   protected!
-  result = true
-  result
-  'Awake'
-end
-
-get '/asleep' do
-  protected!
-  result = false
-  result
-  'Asleep'
+  return 'No that option' if $config['option'][option].nil?
+  result = $config['option'][option]
+  result['text']
 end
